0b3d7fdb5336a2c9b6d4242caed981eea12be5be
116060088 <116060088@ECH-STU-05-21C.SSFSD.LOCAL>
2015-10-16 09:09:46 -0700

    ok

---
+++ src/rPS/RockPaperScissors.java
@@ -0,0 +1,9 @@
+package rPS;
+import java.util.Scanner;
+public class RockPaperScissors {
+	public static Scanner stdin = new Scanner(System.in);
+	public static enum RPS {
+		rock, paper, scissors;
+	}
+	
+}
