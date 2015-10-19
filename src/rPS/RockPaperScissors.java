1d6a5859aced55e531a5d30923e9a313e062f08b
116060088 <alt.people.davidcalman@gmail.com>
2015-10-19 08:20:36 -0700

    TODO: flesh out main method

--- src/rPS/RockPaperScissors.java
+++ src/rPS/RockPaperScissors.java
@@ -1,9 +1,33 @@
 package rPS;
+
 import java.util.Scanner;
+
 public class RockPaperScissors {
 	public static Scanner stdin = new Scanner(System.in);
+
 	public static enum RPS {
-		rock, paper, scissors;
+		rock, paper, scissors, quit;
 	}
-	
+
+	public static RPS scan() {
+		RPS ret = null;
+		while (ret == null) {
+			System.out.print("Choose rock, paper, scissors, or quit: ");
+			String choice = stdin.next();
+			if (choice.equals(RPS.rock.name())) {
+				ret = RPS.rock;
+			} else if (choice.equals(RPS.paper.name())) {
+				ret = RPS.paper;
+			} else if (choice.equals(RPS.scissors.name())) {
+				ret = RPS.scissors;
+			} else { // invalid, quit game
+				ret = RPS.quit;
+			}
+
+		}
+		return ret;
+	}
+	public static void main(String[] args) {
+		
+	}
 }
