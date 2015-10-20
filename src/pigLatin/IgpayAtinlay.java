d8e1c79fe9ecb381004b3628973a066b8d3da8f8
116060088 <alt.people.davidcalman@gmail.com>
2015-10-20 09:04:12 -0700

    added actual program

--- src/pigLatin/IgpayAtinlay.java
+++ src/pigLatin/IgpayAtinlay.java
@@ -1,5 +1,39 @@
 package pigLatin;
 
+import java.util.*;
+
 public class IgpayAtinlay {
-	
+	public static Scanner inputYay = new Scanner(System.in);
+
+	public static enum OnsonantCay {
+		b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z;
+	}
+
+	public static void main(String[] args) {
+		String[] tokens = inputYay.nextLine().split(" ");
+		String[] pigLatinTokens = new String[tokens.length];
+		for (int ii = 0; ii <= tokens.length; ii++) {
+			pigLatinTokens[ii] = ansformTray(tokens[ii]);
+		}
+		System.out.println(Arrays.toString(pigLatinTokens));
+	}
+
+	public static String ansformTray(String okenTay) {
+		String initCons = "";
+		for (int ii = 0; ii < okenTay.length(); ii++) {
+			try {
+				OnsonantCay weedOut = OnsonantCay.valueOf(
+						Character.toString(okenTay.charAt(ii)));
+				initCons += weedOut.name();
+			} catch (IllegalArgumentException e) {
+				break;
+			}
+		}
+		String composedString;
+		if (initCons.equals(""))
+			composedString = okenTay + "yay";
+		else
+			composedString = okenTay.substring(initCons.length()) + initCons + "ay";
+		return composedString;
+	}
 }
